c6781e50b40b3eaab40a5887de6f8cf750d182f4
Jason Claeys <jdclaeys@outlook.com>
2023-04-14 19:33:18 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+//Smooth Scrolling
+$('navbar, a').on('click', function(event){
+    if (this.hash !== '') {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top - 100
+            },
+            1500
+        );
+    }
+});
+
+//Get the current year for the copyright
+$('#year').text(new Date().getFullYear());
